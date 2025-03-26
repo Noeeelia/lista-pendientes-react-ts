370bd270e17fac4a90807b7e21109c74b74b8699
Noelia <noeerivaas@gmail.com>
2025-03-26 18:11:03 -0300

    Se agrego clase flex

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -18,7 +18,7 @@ export const TodoApp = () => {
   return (
     <div>
         <h1>Lista de Tareas</h1>
-        <div>
+        <div className="flex">
             <input type="text"
             value={nuevaTarea}
             onChange={(e) => setNuevaTarea(e.target.value)}
